2dcfd1d01a846e37529cce3793a89489372e5601
marcos galliano <marquitosgalliano@hotmail.com>
2022-09-21 22:21:59 -0300

    desafio 2da entrega

--- index.js
+++ index.js
@@ -134,17 +134,67 @@ if (color === "1"){
 }
  */
 
-let correo = document.getElementById("text");
-let formulario = document.getElementById("formulario");
-formulario.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let inputs = e.target.children;
-    if(!inputs[0].value.includes("@")){
-      inputs[0].value = "";  
-      correo.className = "error";
-      let mensaje = document.createElement("h5");
-      mensaje.innerHTML = "correo invalido"
-      mensaje.className = "rojo";
-      document.body.append(mensaje);
-    }
+// let correo = document.getElementById("text");
+// let formulario = document.getElementById("formulario");
+// formulario.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     let inputs = e.target.children;
+//     if(!inputs[0].value.includes("@")){
+//       inputs[0].value = "";  
+//       correo.className = "error";
+//       let mensaje = document.createElement("h5");
+//       mensaje.innerHTML = "correo invalido"
+//       mensaje.className = "rojo";
+//       document.body.append(mensaje);
+//     }
+// })
+
+
+
+let usuario;
+let usuarioInfo = localStorage.getItem("usuario");
+let h2 = document.getElementById("h2");
+let cuentaDeUsuario = document.getElementById("cuentaDeUsuario");
+
+if(usuarioInfo){
+    let mensaje = `Bienvenid@ ${usuarioInfo}`;
+    h2.innerHTML = `${mensaje}`
+}else{
+    usuario = prompt("ingrese su nombre para crear un usuario");
+    localStorage.setItem("usuario", usuario);
+}
+
+
+for(i = 0; i < 1; i++ ){ 
+let boton = document.getElementById("boton");
+boton.addEventListener("click", () => {
+    let nombreDeUsuario = prompt("ingresar un nombre de usario");
+    let domicilio = prompt("ingrese su domicilio");
+    let sexo = prompt("ingrese su sexo, Hombre o Mujer");
+    let edad = parseInt(prompt("ingrese su edad"));
+        cuentaDeUsuario.innerHTML = `
+                                 <img src="../assets/imagendehombre.png" alt="hombre" class="imgUsuario"> 
+                                 <h4> Nombre de usuario: ${nombreDeUsuario} </h4>
+                                 <h4> Domicilio: ${domicilio} 
+                                 <h4> Edad: ${edad} </h4>
+                                 <h4> sexo: ${sexo} </h4> 
+                                 `   
+    localStorage.setItem("nombreDeUsuario", nombreDeUsuario); 
+    localStorage.setItem("domicilio", domicilio);
+    localStorage.setItem("sexo", sexo);
+    localStorage.setItem("edad", edad);                            
 })
+if(localStorage){
+    let nombreDeUsuario = localStorage.getItem("nombreDeUsuario");
+    let domicilio = localStorage.getItem("domicilio");
+    let sexo = localStorage.getItem("sexo");
+    let edad = localStorage.getItem("edad");
+    cuentaDeUsuario.innerHTML =`
+                                <img src="../assets/imagendehombre.png" alt="hombre" class="imgUsuario"> 
+                                <h4> Nombre de usuario: ${nombreDeUsuario} </h4>
+                                <h4> Domicilio: ${domicilio} 
+                                <h4> Edad: ${edad} </h4>
+                                <h4> sexo: ${sexo} </h4> 
+                                `   
+}
+}
